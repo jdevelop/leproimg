3eb8713019c25e1601dd4d4c2399a5dade6b9163
Eugene Dzhurinsky <jdevelop@gmail.com>
2012-03-13 14:27:47 +0200

    Added WEB server.

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeSynonymInstances #-}
 module Main where
 
 import qualified Text.XML.HXT.XPath as XP
@@ -10,9 +11,35 @@ import qualified Network.Curl.Opts as CO
 import System.Environment
 import System.Directory (getAppUserDataDirectory, doesFileExist)
 
+import Happstack.Server (nullConf, simpleHTTP, FromReqURI(..),ToMessage(..),
+                         toResponse, ok, badRequest, dir, path, flatten)
+import Control.Monad (msum, liftM)
+import Control.Monad.Trans (MonadIO, liftIO)
+import Control.Applicative ((<$>))
+import Data.Maybe
+
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy as BL
+
 appName = "leproimg"
 
 getConfigFileName = getAppUserDataDirectory appName
+
+newtype HashId a = HashId { getHashId :: a }
+
+newtype HTMLResponse a = HTMLResponse { getResponseData :: a }
+
+type HashIdInt = HashId Int
+type HTMLResponseStr = HTMLResponse String
+
+textHtml = C8.pack "text/html"
+
+instance ToMessage HTMLResponseStr where
+  toContentType _ = textHtml
+  toMessage = BL.fromChunks . (:[]) . C8.pack . getResponseData
+
+instance FromReqURI HashIdInt where
+  fromReqURI  = listToMaybe . map (HashId . fst) . reads
 
 extractImgTags content = X.runX $
   X.readString [X.withValidate X.no, X.withParseHTML X.yes] content X.>>>
@@ -21,18 +48,25 @@ extractImgTags content = X.runX $
       ( X.isElem X.>>> X.hasName "img" X.>>> X.getAttrValue "src" )
 
 loadPostPage url leproCookie = do
-  C.initialize
-  (cCode, response) <- C.curlGetString url [ CO.CurlCookie leproCookie ]
+  liftIO C.initialize
+  (cCode, response) <- liftIO $ C.curlGetString url [ CO.CurlCookie leproCookie ]
   if cCode /= CC.CurlOK
-    then fail $ show cCode
-    else extractImgTags response >>= putStrLn . goHtml
+    then return . HTMLResponse $ show cCode
+    else liftIO $ goHtml <$> extractImgTags response
   where
-    goHtml = concatMap (\url -> "<img src=\"" ++ url ++ "\"/>\n")
+    goHtml src = HTMLResponse $ "<html><body>" ++ concatMap (\url -> "<img src=\"" ++ url ++ "\"/>\n") src ++ "</body></html>"
+
+fetchAll leproCookie hashId = flatten $ 
+  loadPostPage ( "http://leprosorium.ru/comments/" ++ show (getHashId hashId :: Int) )
+               leproCookie
 
 main = do
-  (url:_) <- getArgs
   fileNameFromDir <- getConfigFileName
-  go url fileNameFromDir =<< doesFileExist fileNameFromDir
-  where
-    go _ fileNameFromDir False = fail $ "Can not read config file " ++ fileNameFromDir
-    go url fileNameFromDir True = readFile fileNameFromDir >>= loadPostPage url
+  fileExists <- doesFileExist fileNameFromDir
+  leproCookie <- readFile fileNameFromDir
+  if fileExists 
+    then simpleHTTP nullConf $ msum [
+              dir "all" $ path (fetchAll leproCookie),
+              badRequest ( toResponse "Invalid request URL")
+              ]
+    else fail $ "Can not read config file " ++ fileNameFromDir
